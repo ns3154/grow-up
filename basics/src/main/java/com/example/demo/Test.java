1134fefec8a217d3274c3a16ea6780d104dd0830
杨帮东 <397827222@qq.com>
2020-11-20 20:31:51 +0800

    82

--- basics/src/main/java/com/example/demo/Test.java
+++ basics/src/main/java/com/example/demo/Test.java
@@ -19,54 +19,6 @@ public class Test {
 
     public static void main(String[] args) {
 
-        try {
-            int i = 1/0;
-        } catch (Exception e) {
-            logger.error("{},{}", "1", e.getMessage(), e);
-        }
-
-
-
-        ConcurrentHashMap<String, String> map = new ConcurrentHashMap<>();
-        map.put("sss", "ddd");
     }
 
-    private static int test() {
-        int z = 0;
-        try {
-            z++;
-            return z;
-        } finally {
-            z++;
-            System.out.println("finally" + z);
-        }
-    }
-
-    private static Integer test1() {
-        Integer z = new Integer(1);
-        try {
-            return z;
-        } finally {
-            z = 3;
-            System.out.println("finally" + z);
-        }
-    }
-
-    private static Model test2() {
-        Model model = new Model();
-        try {
-            model.setCode(10);
-            return model;
-        } finally {
-            model.setCode(11);
-        }
-    }
-
-    @org.junit.jupiter.api.Test
-    public void sss() {
-        int t = 3;
-        int z = 5;
-        System.out.println(t & z);
-
-    }
 }
