878264015771ac353a592f1c03d0e0ba1d9414be
杨帮东 <yangbd@baojia.com>
2020-12-18 18:53:06 +0800

    92

--- basics/src/main/java/com/example/demo/Test.java
+++ basics/src/main/java/com/example/demo/Test.java
@@ -1,17 +1,10 @@
 package com.example.demo;
 
-import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.SmartInitializingSingleton;
-import org.springframework.boot.CommandLineRunner;
-
 import java.lang.ref.SoftReference;
 import java.lang.ref.WeakReference;
-import java.util.concurrent.Executors;
-import java.util.concurrent.LinkedBlockingDeque;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.HashMap;
 
 /**
  * <pre>
@@ -26,7 +19,13 @@ public class Test {
     private static final Logger logger = LoggerFactory.getLogger(Test.class);
 
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
+
+    }
+
+
+    @org.junit.Test
+    public void reference() {
         Object o = new Object();
         WeakReference<Object> weakReference = new WeakReference<>(o);
         SoftReference<Object> softReference = new SoftReference<>(o);
@@ -51,5 +50,72 @@ public class Test {
 
     }
 
+    @org.junit.Test
+    public void hashMap() {
+        HashMap<Key, String> map = new HashMap<>();
+        Key k1 = new Key(1);
+        Key k2 = new Key(1);
+        Key k3 = new Key(1);
+        Key k4 = new Key(1);
+        Key k5 = new Key(1);
+        Key k6 = new Key(1);
+        Key k7 = new Key(1);
+        Key k8 = new Key(1);
+        Key k9 = new Key(1);
+        Key k10 = new Key(1);
+        Key k11 = new Key(1);
+        map.put(k1, "k1");
+        map.put(k2, "k2");
+        map.put(k3, "k3");
+        map.put(k4, "k4");
+        map.put(k5, "k5");
+        map.put(k6, "k6");
+        map.put(k7, "k7");
+        map.put(k8, "k8");
+        map.put(k9, "k9");
+        map.put(k10, "k10");
+        map.put(k11, "k11");
+    }
+
+
+    static class Key {
+
+        private Integer num;
+
+        public Key() {
+            // nothing
+        }
+
+        public Key(Integer num) {
+            this.num = num;
+        }
+
+        public Integer getNum() {
+            return num;
+        }
+
+        public void setNum(Integer num) {
+            this.num = num;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (!(o instanceof Key)) {
+                return false;
+            }
+
+            Key key = (Key) o;
+
+            return num != null ? num.equals(key.num) : key.num == null;
+        }
+
+        @Override
+        public int hashCode() {
+            return num != null ? num.hashCode() : 0;
+        }
+    }
 
 }
