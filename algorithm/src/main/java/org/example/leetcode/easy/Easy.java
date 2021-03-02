3db96080dd1720144d100bbe91ad1ea6c7ee0a09
杨帮东 <397827222@qq.com>
2021-03-03 00:22:12 +0800

    sdfsfsafsa

--- algorithm/src/main/java/org/example/leetcode/easy/Easy.java
+++ algorithm/src/main/java/org/example/leetcode/easy/Easy.java
@@ -3,6 +3,7 @@ package org.example.leetcode.easy;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -33,4 +34,50 @@ public class Easy {
         }
         System.out.println(max);
     }
+
+	@Test
+	public void lengthOfLongestSubstring1() {
+		String s = "pwwkew";
+		char[] chars = s.toCharArray();
+		int[] nums = new int[128];
+		int max = 0;
+		for (int start = 0, end = 0, length = chars.length; end < length; end++) {
+			char c = chars[end];
+			start = Math.max(nums[c], start);
+			nums[c] = end + 1;
+			max = Math.max(max, end - start + 1);
+		}
+		System.out.println(max);
+	}
+
+	@Test
+	public void romanToInt() {
+		String s = "MCMXCIV";
+		int sum = 0;
+		int preNum = getValue(s.charAt(0));
+		for(int i = 1;i < s.length(); i ++) {
+			int num = getValue(s.charAt(i));
+			if(preNum < num) {
+				sum -= preNum;
+			} else {
+				sum += preNum;
+			}
+			preNum = num;
+		}
+		sum += preNum;
+		System.out.println(sum);
+	}
+
+	private int getValue(char ch) {
+		switch(ch) {
+			case 'I': return 1;
+			case 'V': return 5;
+			case 'X': return 10;
+			case 'L': return 50;
+			case 'C': return 100;
+			case 'D': return 500;
+			case 'M': return 1000;
+			default: return 0;
+		}
+	}
 }
--- algorithm/src/main/java/org/example/leetcode/easy/twoSum.java
+++ algorithm/src/main/java/org/example/leetcode/easy/twoSum.java
@@ -3,6 +3,10 @@ package org.example.leetcode.easy;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
 /**
  * <pre>
  *  给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
@@ -21,22 +25,44 @@ public class twoSum {
 
     @Test
     public void test() {
-        System.out.println(reverse(-10));
+		int[] nums = {3,2,4};
+		int target = 6;
+
+		System.out.println(twoSum(nums, target));
+
     }
 
-    public int reverse(int x) {
-        int res = 0;
-        while(x!=0) {
-            int tmp = x%10;
-            if (res>214748364 || (res==214748364 && tmp>7)) {
-                return 0;
-            }
-            if (res<-214748364 || (res==-214748364 && tmp<-8)) {
-                return 0;
-            }
-            res = res*10 + tmp;
-            x /= 10;
-        }
-        return res;
-    }
+	public int[] twoSum(int[] nums, int target) {
+		Map<Integer, Integer> map = new HashMap<>();
+		for (int i = 0;i <nums.length;i++) {
+			int z = nums[i];
+			int t = target - z;
+			Integer index1 = map.get(t);
+
+			if (null != index1) {
+				return new int[] {index1, i};
+			}
+			map.put(z, i);
+		}
+		return new int[]{};
+	}
+
+	// a b c d e a b c d e n a f g z s q a
+	public int lengthOfLongestSubstring(String s) {
+		int max = 0;
+		int left = 0;
+		int arraylen = s.length();
+		HashMap<Character, Integer> map = new HashMap<>();
+		char[] chars = s.toCharArray();
+		for (int i = 1; i<= arraylen;i++) {
+			char c = chars[i];
+			if (map.containsKey(c)) {
+				left = Math.max(left, map.get(c) + 1);
+			}
+
+			map.put(chars[i], i);
+		}
+		return max;
+	}
+
 }
