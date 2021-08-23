1913d04c55ed687e5afeb9f72978b6d13d5bc3fc
ybd <397827222@qq.com>
2021-08-23 23:12:47 +0800

    new page

--- wallpaper/src/main/java/com/example/wallpaper/netbian/Main.java
+++ wallpaper/src/main/java/com/example/wallpaper/netbian/Main.java
@@ -310,7 +310,7 @@ public class Main {
     // 179
     public static void main(String[] args) throws InterruptedException {
 
-        int start = 21;
+        int start = 41;
         int end = start + 5;
         down("4kyouxi", start, end);
     }
