e1ebb67f00db0a4460534cbcad912cbe51971c39
杨帮东 <yangbd@baojia.com>
2021-12-03 15:05:55 +0800

    LISTED

---
+++ algorithm/src/main/java/org/example/algorithm/leecode/model/Node.java
@@ -0,0 +1,37 @@
+package org.example.algorithm.leecode.model;
+
+/**
+ * <p>
+ *     desc
+ * </p>
+ * @author 杨帮东
+ * @since 1.0
+ * @date 2021/12/03 10:59
+ **/
+public class Node {
+    public int val;
+    public Node next;
+
+    public Node () {
+    }
+
+    public Node (int _val) {
+        val = _val;
+    }
+
+    public Node (int _val, Node _next) {
+        val = _val;
+        next = _next;
+    }
+
+    public static Node build(int[] ints) {
+        Node node = new Node(ints[0]);
+        Node cur = node;
+        for (int i = 1; i < ints.length; i++) {
+            node = node.next = new Node(ints[i]);
+        }
+        node.next = cur;
+        return cur;
+    }
+
+}
