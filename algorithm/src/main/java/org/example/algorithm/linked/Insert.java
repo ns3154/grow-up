91b3aebc9751d7c4ad6e70af0c0d26928167bab9
杨帮东 <yangbd@baojia.com>
2021-12-03 15:05:17 +0800

    LISTED

---
+++ algorithm/src/main/java/org/example/algorithm/linked/Insert.java
@@ -0,0 +1,63 @@
+package org.example.algorithm.linked;
+
+import org.example.algorithm.leecode.model.Node;
+import org.junit.jupiter.api.Test;
+
+/**
+ * <p>
+ *     desc
+ * </p>
+ * @author 杨帮东
+ * @since 1.0
+ * @date 2021/12/03 10:59
+ **/
+public class Insert {
+
+    @Test
+    public void test () {
+        Node node = Node.build(new int[] {1,3,5});
+        Node insert = insert(node, 2);
+        System.out.println(node);
+    }
+
+    public Node insert (Node head, int insertVal) {
+        if (null == head) {
+            Node node = new Node(insertVal);
+            node.next = node;
+            return node;
+        }
+
+        Node max = head;
+        Node min = head;
+        Node cur = head.next;
+        while (head != cur) {
+            if (max.val <= cur.val) {
+                max = cur;
+            }
+            if (min.val > cur.val) {
+                min = cur;
+            }
+            cur = cur.next;
+        }
+
+        Node node = new Node(insertVal);
+        if (insertVal >= max.val || insertVal <= min.val) {
+            node.next = max.next;
+            max.next = node;
+        } else {
+            cur = head;
+            while (true) {
+                if (insertVal >= cur.val && insertVal <= cur.next.val) {
+                    node.next = cur.next;
+                    cur.next = node;
+                    break;
+                }
+                cur = cur.next;
+            }
+        }
+
+        return head;
+    }
+
+
+}
