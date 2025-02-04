fbcaac030c6bb4b51271ec4fa78f1444b05f9893
yang <397827222@qq.com>
2025-02-04 13:22:24 +0800

    refactor(mvc): 移除未使用的导入语句
    - 删除了 TestA、Controller、ConfigurableListableBeanFactory 和 ConfigurableApplicationContext 等未使用的导入语句
    - 简化了代码结构，提高了代码的可读性和维护性

--- mvc/src/main/java/com/example/mvc/MvcApplication.java
+++ mvc/src/main/java/com/example/mvc/MvcApplication.java
@@ -1,11 +1,7 @@
 package com.example.mvc;
 
-import com.example.mvc.config.bean.TestA;
-import com.example.mvc.controller.Controller;
-import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class MvcApplication {
